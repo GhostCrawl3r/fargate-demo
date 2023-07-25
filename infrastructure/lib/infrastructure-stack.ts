a50147db039de45dc18cf4b057107bf0398ba650
Nick Morgan <nick@interstellaragency.co.uk>
2023-07-25 15:14:16 +0100

    pipeline changes

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -3,29 +3,34 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class FargateDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const repository = new ecr.Repository(this, 'fargate-demo-repository', {
+      repositoryName: 'fargate-demo-repository',
+    });
+
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 3, // Default is all AZs in region
     });
 
-    const cluster = new ecs.Cluster(this, 'MyCluster', {
+    const cluster = new ecs.Cluster(this, 'Fargate-Demo-Cluster', {
       vpc: vpc,
     });
 
     // Create a load-balanced Fargate service and make it public
     new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
-      'MyFargateService',
+      'FargateDemoService',
       {
         cluster: cluster, // Required
         cpu: 512, // Default is 256
         desiredCount: 6, // Default is 1
         taskImageOptions: {
-          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+          image: ecs.EcrImage.fromEcrRepository(repository, 'latest'),
         },
         memoryLimitMiB: 2048, // Default is 512
         publicLoadBalancer: true, // Default is true
